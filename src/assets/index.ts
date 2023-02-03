4e7fcc96927ba54623131545682f63fb6aaad38d
Arseniy Rybalov <ar@crypto.rocks>
2023-02-03 12:32:14 +0600

    feat: configure project

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import settingsSVG from "./settings.svg";
+import closeSVG from "./close.svg";
+import checkSVG from "./check.svg";
+import arrowSVG from "./arrow.svg";
+
+export { settingsSVG, closeSVG, checkSVG, arrowSVG };
